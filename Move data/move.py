9a3e5dd81f1c4790295f0f9553a9a6a180cce93a
michaelrichie <michaelrichierr7@gmail.com>
2021-05-05 08:53:37 +0700

    move to main

---
+++ Move data/move.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import openpyxl
+
+
+with open('C:\Users\919008036@ot.id\Desktop\Project Server Monitoring\read server','r') as read:
+	reading.read(test.txt)
+
+import pandas as pd
+df = pd.read_csv('input.csv') # can replace with df = pd.read_table('input.txt') for '\t'
+df.to_excel('output.xlsx', 'Sheet1')
